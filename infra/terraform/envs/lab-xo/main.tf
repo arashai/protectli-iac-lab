c0af6a6f64affaffc8cda8aba8cb6158464bff23
Chris Dyck <arashai.bluewolf@gmail.com>
2026-02-07 00:12:38 -0700

    Implement Xen Orchestra integration with variable definitions and resource configurations

--- infra/terraform/envs/lab-xo/main.tf
+++ infra/terraform/envs/lab-xo/main.tf
@@ -13,13 +13,61 @@ provider "xenorchestra" {
   url      = var.XOA_URL
   username = var.XOA_USER
   password = var.XOA_PASSWORD
-  insecure = var.XOA_INSECURE == "true"
+  insecure = var.XOA_INSECURE
 }
 
-data "xenorchestra_pool" "localhost" {
-  name_label = "localhost"
+data "xenorchestra_pool" "lab" {
+  name_label = var.pool_name
 }
 
-output "pool_names" {
-  value = data.xenorchestra_pool.localhost.id
+# --- Network discovery ---
+data "xenorchestra_network" "lan" {
+  name_label = var.network_name
+  pool_id    = data.xenorchestra_pool.lab.id
+}
+output "network_id" {
+  value = data.xenorchestra_network.lan.id
+}
+
+# --- Template discovery ---
+data "xenorchestra_template" "base" {
+  name_label = var.template_name
+  pool_id    = data.xenorchestra_pool.lab.id
+}
+output "template_id" {
+  value = data.xenorchestra_template.base.id
+}
+
+# --- Storage discovery ---
+data "xenorchestra_sr" "main" {
+  name_label = var.sr_name
+  pool_id    = data.xenorchestra_pool.lab.id
+}
+
+output "sr_id" {
+  value = data.xenorchestra_sr.main.id
+}
+
+resource "xenorchestra_vm" "test" {
+  name_label  = var.vm_name
+  template    = data.xenorchestra_template.base.id
+  cpus        = 1
+  memory_max  = 1073741824 # 1 GiB
+  power_state = "Halted"
+
+  network {
+    network_id = data.xenorchestra_network.lan.id
+  }
+
+  disk {
+    sr_id      = data.xenorchestra_sr.main.id
+    name_label = "root"
+    size       = 10737418240 # 10 GiB
+  }
+
+  tags = ["terraform", "lab", "test"]
+}
+
+output "vm_id" {
+  value = xenorchestra_vm.test.id
 }
--- infra/terraform/envs/lab-xo/variables.tf
+++ infra/terraform/envs/lab-xo/variables.tf
@@ -7,7 +7,7 @@ variable "XOA_URL" {
 variable "XOA_USER" {
   description = "Service Account Username for XOA"
   type        = string
-  sensitive   = true
+  sensitive   = false
 }
 
 variable "XOA_PASSWORD" {
@@ -19,5 +19,27 @@ variable "XOA_PASSWORD" {
 variable "XOA_INSECURE" {
   description = "Whether to skip SSL verification"
   type        = string
-  sensitive   = true
+  sensitive   = false
+}
+
+variable "network_name" {
+  type        = string
+  description = "XO network name_label to attach VMs to (e.g. Pool-wide network associated with eth0)"
+}
+
+variable "pool_name" {
+  type        = string
+  description = "XO pool name_label to use (e.g. localhost)"
+}
+variable "template_name" {
+  type        = string
+  description = "XO template name_label to clone from (e.g., Ubuntu 24.04 Cloud-Init)"
+}
+variable "sr_name" {
+  type        = string
+  description = "Storage Repository name_label for VM disks"
+}
+variable "vm_name" {
+  type        = string
+  description = "Name of the VM to be created"
 }
